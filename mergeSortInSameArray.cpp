2bb645c888ea5d8e4664932f7649f53680d55a31
DevMehta13 <139580977+DevMehta13@users.noreply.github.com>
2023-10-30 12:12:27 +0530

    Add files via upload

---
+++ mergeSortInSameArray.cpp
@@ -0,0 +1,13 @@
+void merge(int* nums1, int nums1Size, int m, int* nums2, int nums2Size, int n){
+
+int i = m - 1;
+int j = n - 1;
+int k = m + n - 1;
+while(j>=0){
+if(i>=0 && nums1[i] > nums2[j])
+    nums1[k--] = nums1[i--];
+else
+    nums1[k--] = nums2[j--];
+}
+
+}
